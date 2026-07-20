a4b3ff4706a82d59bb8210e266271f939f831780
CruzInc <abstractime@outlook.com>
2026-07-20 10:08:47 -0600

    Project Creation

---
+++ src/main/java/com/liverpool/liverpool_dev/LiverpoolDevApplication.java
@@ -0,0 +1,13 @@
+package com.liverpool.liverpool_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiverpoolDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiverpoolDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liverpool/liverpool_dev/LiverpoolDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liverpool.liverpool_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiverpoolDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
